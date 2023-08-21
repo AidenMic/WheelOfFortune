2d04d3ca9d2db92b3e62e81f96b57a340a195f2d
Aiden <aidenmic@msn.com>
2023-08-21 16:52:54 -0500

    Added log showing letters already guessed

--- puzzleCode.js
+++ puzzleCode.js
@@ -4,30 +4,34 @@ function puzzleHide(puzzleDiv) {
 	return puzzle.toLocaleLowerCase();
 }
 
-function guessLetter(event, puzzle, puzzleDiv) {
+function guessLetter(event, puzzle, puzzleDiv, prevLetters) {
 	let inputSpace = event.currentTarget;
 	inputSpace.disabled = true;
 	inputSpace.value = ``;
 	let inputChar = event.key.toLocaleLowerCase();
 	let numValue = 0;
 	
-	if (
-		(inputChar.length == 1) && 
-		(puzzle.includes(inputChar))
-		)
-	{
-		console.log(`yippee: ` + inputChar);
-		let puzzleIndex = -1;
-		while (true)
+	if (!prevLetters.innerHTML.includes(inputChar)) {
+		prevLetters.innerHTML = prevLetters.innerHTML + inputChar;
+	
+		if (
+			(inputChar.length == 1) && 
+			(puzzle.includes(inputChar))
+			)
 		{
-			puzzleIndex++;
-			puzzleIndex = puzzle.indexOf(inputChar, puzzleIndex);
-			if (puzzleIndex !== -1) {
-				puzzleDiv.innerHTML = puzzleDiv.innerHTML.substring(0, puzzleIndex) + inputChar + puzzleDiv.innerHTML.substring(puzzleIndex + 1);
-				numValue++;
-			}
-			else {
-				break;
+			console.log(`yippee: ` + inputChar);
+			let puzzleIndex = -1;
+			while (true)
+			{
+				puzzleIndex++;
+				puzzleIndex = puzzle.indexOf(inputChar, puzzleIndex);
+				if (puzzleIndex !== -1) {
+					puzzleDiv.innerHTML = puzzleDiv.innerHTML.substring(0, puzzleIndex) + inputChar + puzzleDiv.innerHTML.substring(puzzleIndex + 1);
+					numValue++;
+				}
+				else {
+					break;
+				}
 			}
 		}
 	}
@@ -37,9 +41,11 @@ function guessLetter(event, puzzle, puzzleDiv) {
 
 let puzzle = "";
 let puzzleDiv;
+let prevLetters;
 
 
 window.onload = (event) => {
 	puzzleDiv = document.getElementById("puzzle");
+	prevLetters = document.getElementById("prevLetters");
 	puzzle = puzzleHide(puzzleDiv);
 };
