910d4b07dd5780b467404ede7d0ba530f86d25fd
Aiden <aidenmic@msn.com>
2023-08-20 20:51:11 -0500

    Added Guess Letter Ability that also checks for how many instances of that letter there are

--- puzzleCode.js
+++ puzzleCode.js
@@ -1,7 +1,37 @@
 function puzzleHide(puzzleDiv) {
 	let puzzle = puzzleDiv.innerHTML;
 	puzzleDiv.innerHTML = puzzle.replace(/\S/gm, '_');
-	return puzzle;
+	return puzzle.toLocaleLowerCase();
+}
+
+function guessLetter(event, puzzle, puzzleDiv) {
+	let inputSpace = event.currentTarget;
+	inputSpace.disabled = true;
+	inputSpace.value = ``;
+	let inputChar = event.key.toLocaleLowerCase();
+	let numValue = 0;
+	
+	if (
+		(inputChar.length == 1) && 
+		(puzzle.includes(inputChar))
+		)
+	{
+		console.log(`yippee: ` + inputChar);
+		let puzzleIndex = -1;
+		while (true)
+		{
+			puzzleIndex++;
+			puzzleIndex = puzzle.indexOf(inputChar, puzzleIndex);
+			if (puzzleIndex !== -1) {
+				numValue++;
+			}
+			else {
+				break;
+			}
+		}
+	}
+	console.log(numValue);
+	inputSpace.disabled = false;
 }
 
 let puzzle = "";
